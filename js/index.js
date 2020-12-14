6790abbaa7a51764b7328f42286d65f8ebb8979f
Bolyki Balázs <bbalage0@gmail.com>
2020-12-14 20:09:43 +0100

    Generalized loading for cars and manufacturers. Update page of cars loads.

--- js/index.js
+++ js/index.js
@@ -33,3 +33,59 @@ $(document).ready(function (){
         })
     })
 })
+
+function loadToTable(url, tableId, updatePage){
+    $.getJSON(url, function (data){
+        var tableBody = $("#"+tableId).find("tbody")[0];
+        $.each(data, function(key, value){
+            const propertyNames = Object.getOwnPropertyNames(value);
+            const row = $("<tr></tr>");
+            const rowId = value._id;
+            $(row).attr("id", rowId);
+            for(let i = 1; i < propertyNames.length; i++){
+                let generalCell = $("<td>"+value[propertyNames[i]]+"</td>");
+                row.append(generalCell);
+            }
+            /*Cell for update button.*/
+            const updateCell = $("<td></td>");
+            const updateButton = $("<button>Update</button>");
+            //$(updateButton).attr("id",value._id+"UpdateButton");
+            $(updateButton).attr("class", "updateButton");
+            $(updateButton).click(function (event) {
+                event.preventDefault();
+                updatedObject = {
+                    "id": value._id
+                };
+                for(let i = 1; i < propertyNames.length; i++){
+                    updatedObject[propertyNames[i]] = value[propertyNames[i]];
+                }
+                $("#contentSection").load(updatePage);
+            });
+            $(updateCell).append(updateButton);
+            $(row).append(updateCell);
+            $(tableBody).append(row);
+            /*Adding delete button*/
+            const deleteCell = $("<td></td>");
+            const deleteButton = $("<button>Delete</button>");
+            //$(deleteButton).attr("id",value._id+"DeleteButton");
+            $(deleteButton).attr("class", "deleteButton");
+            $(deleteButton).click(function (event) {
+                event.preventDefault();
+                const urlOrder = url + "/" + value._id;
+                console.log(urlOrder);
+                $.ajax(
+                    {
+                        url: urlOrder,
+                        dataType: "json",
+                        type: "DELETE"
+                    }
+                );
+                $("#"+rowId).remove();
+                //$("#contentSection").load("manufacturers.html"); //Load happens faster, than deletion in database.
+            });
+            $(deleteCell).append(deleteButton);
+            $(row).append(deleteCell);
+            $(tableBody).append(row);
+        })
+    });
+}
