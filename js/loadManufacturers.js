77134d29075fad1a44d923ac314ed26bd245dce5
Bolyki Balázs <bbalage0@gmail.com>
2020-12-14 19:17:57 +0100

    Deletion of manufacturers added.

--- js/loadManufacturers.js
+++ js/loadManufacturers.js
@@ -2,17 +2,19 @@ $(document).ready(function (){
     $.getJSON("https://webtechcars.herokuapp.com/api/manufacturers", function (data){
         var tableBody = $("#manufacturerListTable").find("tbody")[0];
         $.each(data, function(key, value){
-            var row = $("<tr></tr>");
-            var nameCell = $("<td>"+value.name+"</td>");
-            var countryCell = $("<td>"+value.country+"</td>");
-            var foundedCell = $("<td>"+value.founded+"</td>");
+            const row = $("<tr></tr>");
+            const rowId = value._id;
+            $(row).attr("id", rowId);
+            const nameCell = $("<td>"+value.name+"</td>");
+            const countryCell = $("<td>"+value.country+"</td>");
+            const foundedCell = $("<td>"+value.founded+"</td>");
             $(row).append(nameCell);
             $(row).append(countryCell);
             $(row).append(foundedCell);
             /*Cell for update button.*/
-            var formUpdateCell = $("<td></td>");
-            var updateButton = $("<button>Update</button>");
-            $(updateButton).attr("id",value._id+"Button");
+            const updateCell = $("<td></td>");
+            const updateButton = $("<button>Update</button>");
+            //$(updateButton).attr("id",value._id+"UpdateButton");
             $(updateButton).attr("class", "updateButton");
             $(updateButton).click(function (event) {
                 event.preventDefault();
@@ -23,15 +25,32 @@ $(document).ready(function (){
                     "country": value.country,
                     "founded": value.founded
                 };
-            })
-            $(formUpdateCell).append(updateButton);
-            $(row).append(formUpdateCell);
+            });
+            $(updateCell).append(updateButton);
+            $(row).append(updateCell);
+            $(tableBody).append(row);
+            /*Adding delete button*/
+            const deleteCell = $("<td></td>");
+            const deleteButton = $("<button>Delete</button>");
+            //$(deleteButton).attr("id",value._id+"DeleteButton");
+            $(deleteButton).attr("class", "deleteButton");
+            $(deleteButton).click(function (event) {
+                event.preventDefault();
+                const urlOrder = "https://webtechcars.herokuapp.com/api/manufacturers/" + value._id;
+                console.log(urlOrder);
+                $.ajax(
+                    {
+                        url: urlOrder,
+                        dataType: "json",
+                        type: "DELETE"
+                    }
+                );
+                $("#"+rowId).remove();
+                //$("#contentSection").load("manufacturers.html"); //Load happens faster, than deletion in database.
+            });
+            $(deleteCell).append(deleteButton);
+            $(row).append(deleteCell);
             $(tableBody).append(row);
         })
     });
-    /*$(".updateButton").each(function () {
-        $(this).click(function (event) {
-
-        })
-    })*/
 })
